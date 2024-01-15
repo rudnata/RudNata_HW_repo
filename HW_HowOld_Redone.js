690bb63d02eb9fb2749d98917300f6d5518a866f
rudnata <155205582+rudnata@users.noreply.github.com>
2024-01-15 18:01:29 -0500

    Update HW_HowOld_Redone.js

--- HW_HowOld_Redone.js
+++ HW_HowOld_Redone.js
@@ -3,12 +3,9 @@ if (isNaN(year) || year < 0 || year==="") {
     console.log("Рік введено невірно.")
 } 
 else {
-    
-    if (year ===1 || (year % 10===1 && +year!==11)) 
-    {
+    if (year ===1 || (year % 10===1 && +year!==11)) {
     console.log(year + " рік.");}
-    else if ((year >=2 && year <=4) || (year % 10>=2 && year % 10<=4 && year>14)) 
-    {
+    else if ((year >=2 && year <=4) || (year % 10>=2 && year % 10<=4 && year>14)) {
     console.log(year + " роки.");}
     else {
     console.log(year + " років.");}
